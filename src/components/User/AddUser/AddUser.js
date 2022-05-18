c64f30e119f76e0f8420bcc61329fce7d10898a2
AdnanAli <aa@asappz.com>
2022-05-18 09:29:12 +0200

    implemented ErrorModal component

--- src/components/User/AddUser/AddUser.js
+++ src/components/User/AddUser/AddUser.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import Card from "../../UI/Card/Card";
 import styles from './AddUser.module.css'
 import Button from "../../UI/Button/Button";
+import ErrorModal from "../../UI/ErrorModal/ErrorModal";
 
 const AddUser = props => {
 
@@ -11,7 +12,7 @@ const AddUser = props => {
     const addUserHandler = (event) => {
         event.preventDefault();
         if(enteredUsername.trim().length === 0 || userAge.trim().length === 0 || +userAge < 1) {
-            return
+
         } else {
             props.onAddUser(enteredUsername, userAge)
             setEnteredUsername('');
@@ -28,15 +29,18 @@ const AddUser = props => {
     }
 
     return (
-        <Card className={styles.input}>
-            <form onClick={addUserHandler}>
-                <label htmlFor="username">User name</label>
-                <input id="username" type="text" value={enteredUsername} onChange={usernameChangeHandler}/>
-                <label htmlFor="age">Age (years)</label>
-                <input id="age" type="number" value={userAge} onChange={ageChangeHandler}/>
-                <Button>Add user</Button>
-            </form>
-        </Card>
+        <div>
+            <ErrorModal title={'An Error'} message={'Invalid input!'}/>
+            <Card className={styles.input}>
+                <form onClick={addUserHandler}>
+                    <label htmlFor="username">User name</label>
+                    <input id="username" type="text" value={enteredUsername} onChange={usernameChangeHandler}/>
+                    <label htmlFor="age">Age (years)</label>
+                    <input id="age" type="number" value={userAge} onChange={ageChangeHandler}/>
+                    <Button>Add user</Button>
+                </form>
+            </Card>
+        </div>
     )
 }
 
